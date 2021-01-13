e430dc51a62924ebf7bf09215cfdb1c4e2936899
imrafaelmerino <imrafael.merino@gmail.com>
2021-01-13 22:26:23 +0100

    feat: 🎸 compose method

--- src/main/java/vertx/effect/λc.java
+++ src/main/java/vertx/effect/λc.java
@@ -14,12 +14,12 @@ public interface λc<I, O> extends BiFunction<MultiMap, I, Val<O>> {
     MultiMap empty = MultiMap.caseInsensitiveMultiMap();
 
     static <I> λc<I, I> identity() {
-        return (context,val)->Cons.success(val);
+        return (context, val) -> Cons.success(val);
     }
 
     static <I, O> λc<I, O> fail(final Supplier<Exception> supplier) {
         Objects.requireNonNull(supplier);
-        return (context,val) ->
+        return (context, val) ->
                 Cons.failure(supplier.get());
     }
 
@@ -43,21 +43,33 @@ public interface λc<I, O> extends BiFunction<MultiMap, I, Val<O>> {
                          );
     }
 
+    default <Q> λc<Q, O> compose(final λc<Q, I> other) {
+        requireNonNull(other);
+        return (context, input) -> {
+            if (input == null)
+                return Cons.failure(new IllegalArgumentException("input is null"));
+            return other.apply(context,
+                               input
+                              )
+                        .flatMap(i -> this.apply(context,
+                                                 i
+                                                )
+                                );
+        };
+    }
+
     default <Q> λc<I, Q> andThen(final λc<O, Q> other) {
         requireNonNull(other);
         return (context, input) -> {
             if (input == null)
                 return Cons.failure(new IllegalArgumentException("input is null"));
-            return Cons.of(() -> this.apply(context,
-                                            input
-                                           )
-                                     .get()
-                                     .flatMap(it -> other.apply(context,
-                                                                it
-                                                               )
-                                                         .get()
-                                             )
-                          );
+            return this.apply(context,
+                              input
+                             )
+                       .flatMap(it -> other.apply(context,
+                                                  it
+                                                 )
+                               );
         };
     }
 
